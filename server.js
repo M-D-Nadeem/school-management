79ae4b8485ee90862b1b85d0f34df93fa8528bfe
M-D-Nadeem <149703432+M-D-Nadeem@users.noreply.github.com>
2024-11-23 01:51:19 +0530

    first commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config();
 app.use(bodyParser.json());
 app.use("/api",router);
 
-const PORT=process.env.PORT || 3001;
+const PORT=process.env.PORT || 3002;
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
