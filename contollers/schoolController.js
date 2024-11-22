2a7e0fc3da8d2af0789e0d4776f57227ceced696
M-D-Nadeem <149703432+M-D-Nadeem@users.noreply.github.com>
2024-11-23 01:20:05 +0530

    first commit

---
+++ contollers/schoolController.js
@@ -0,0 +1,41 @@
+import connection from "../connect.js";
+import haversine from "haversine-distance"
+const addSchool=(req,res)=>{
+    const { name, address, latitude, longitude } = req.body;
+
+    if(!name || !address || !latitude || !longitude){
+        return res.status(400).json({ error: 'Invalid input data' });
+    }
+    const query=`insert into school(name,address,latitude,longitude) values (?, ?, ?, ?);`
+    connection.query(query,[name, address, latitude, longitude],(err,result)=>{
+        if (err) {
+            return res.status(500).json({ error: 'Failed to add the school' });
+          }
+          return res.status(200).json({ message: 'School added successfully', id: result.insertId })
+    })
+
+}
+const listSchools=(req,res)=>{
+    const userLatitude = parseFloat(req.query.latitude);
+    const userLongitude = parseFloat(req.query.longitude);
+
+    if(isNaN(userLatitude) || isNaN(userLongitude)){
+        return res.status(400).json({ error: 'Invalid user coordinates' });
+    }
+    const query="Select * from school;"
+    connection.query((query),(err,schools)=>{
+        if (err) {
+            return res.status(500).json({ error: 'Failed to retrieve schools' });
+          }
+        const sortedSchool=schools.map((school)=>{
+            const schoolCoordinates = { lat: school.latitude, lon: school.longitude };
+            const userCoordinates = { lat: userLatitude, lon: userLongitude };
+            school.distance = haversine(userCoordinates, schoolCoordinates) /1000;
+            return school;
+        }).sort((a, b) => a.distance - b.distance);
+
+        res.json(sortedSchool);
+    })
+}
+
+export {addSchool,listSchools}
---
+++ server.js
@@ -0,0 +1,16 @@
+import bodyParser from "body-parser";
+import dotenv from "dotenv"
+import express from "express"
+import router from "./routers.js";
+
+const app=express();
+dotenv.config();
+
+app.use(bodyParser.json());
+app.use("/api",router);
+
+const PORT=process.env.PORT || 3001;
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
